b8e57d0c7ac183d84059b62f45dc6ba8f3dff5a7
ben <benfd@outlook.com>
2020-10-04 20:21:12 +0100

    20:20, 04/10/20 - Finished shop class, starting tests

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,52 @@
+import behaviours.ISell;
+
+import java.util.List;
+
 public class Shop {
+
+    // Attributes
+    private String shopName;
+    private List<ISell> stock;
+    private double till;
+
+    // Constructor
+
+    public Shop(String shopName, double till) {
+        this.shopName = shopName;
+        this.till = till;
+    }
+
+    // Methods
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public List<ISell> getStock() {
+        return stock;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public void setTill(double till) {
+        this.till = till;
+    }
+
+    public void addToStock(ISell item) {
+        this.stock.add(item);
+    }
+
+    public void removeFromStock(ISell item) {
+        this.stock.remove(item);
+    }
+
+    public double calculateTotalPotentialProfit() {
+        double calculated = this.stock.stream()
+                .map(item -> item.getMarkup())
+                .reduce(0.00, (sum, profit) -> sum += profit);
+
+        return calculated;
+    }
 }
